21f493aba787299d0b77b818cbe1b4a82f1c2626
smitdaddy <smitpatildpsv@gmail.com>
2026-05-21 17:38:33 +0530

    Fix AdminDashboard Link import and convert anchor tags to React Router Links

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { usePatients } from '../context/PatientContext';
 
 export default function AdminDashboard() {
